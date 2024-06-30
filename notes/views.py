39233204de1ffc92998f7d30afb15b3ac6aee78c
zadvajr <danielzadva@gmail.com>
2024-06-30 20:18:09 +0100

    adding local repo

---
+++ notes/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import Http404
+from . models import Notes
+
+# Create your views here.
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {
+        'notes': all_notes,
+    })
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note Does Not Exist!")
+    return render(request, 'notes/notes_detail.html', {'note':note})
